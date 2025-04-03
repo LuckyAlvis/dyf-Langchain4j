70e40763648751abcb4e161c0372a9d55728a732
ivan <252127196@qq.com>
2025-04-03 16:25:10 +0800

    init

---
+++ src/main/java/com/shenzhen/dai/langchain4j/DyfLangchain4jApplication.java
@@ -0,0 +1,13 @@
+package com.shenzhen.dai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyfLangchain4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DyfLangchain4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shenzhen/dai/langchain4j/DyfLangchain4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenzhen.dai.langchain4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyfLangchain4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
