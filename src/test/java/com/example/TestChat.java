956e64027c2d9a5bd4320a0c467f6450e0832dd5
ivan <252127196@qq.com>
2025-04-03 18:37:08 +0800

    feature: ollama对话

--- src/test/java/com/example/TestChat.java
+++ src/test/java/com/example/TestChat.java
@@ -1,11 +1,15 @@
 package com.example;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
 
 public class TestChat {
 
+    /**
+     * 测试chatgpt
+     */
     @Test
     void test01() {
         ChatLanguageModel model = OpenAiChatModel
@@ -17,4 +21,21 @@ public class TestChat {
         String answer = model.chat("你好，你是谁？");
         System.out.println(answer);
     }
+
+    /**
+     * 测试ollama
+     */
+    @Test
+    void test02() {
+        ChatLanguageModel model = OllamaChatModel
+                .builder()
+                .baseUrl("http://localhost:11434")
+                .modelName("deepseek-r1:7b")
+                .build();
+
+        String answer = model.chat("你好，你是谁？");
+        System.out.println(answer);
+    }
+
+
 }
